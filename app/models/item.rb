f7f59c983c478f7312185de47bf53ec91b2793a9
Lindsay Taylor <ltaylor2@chicagobooth.edu>
2014-11-30 22:52:03 -0600

    Final evening changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ActiveRecord::Base
   belongs_to :category
+  belongs_to :reservation
   validates :name, :presence => true
   validates :category, :presence => true
   validates :size, :presence => true
   validates :color, :presence => true
   validates :price, :presence => true
+  validates :reservation, :presence => true
 end
