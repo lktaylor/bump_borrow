d88419341b9a98c6682f02e61384b31621a8a8ea
Lindsay Taylor <ltaylor2@chicagobooth.edu>
2014-12-01 17:38:19 -0600

    Additional changes

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,5 @@
 class Reservation < ActiveRecord::Base
   belongs_to :item
   validate :availability
-  def availability check for reserve_start and reserve_end if item = x
-end
+  # def availability --- check for reserve_start and reserve_end if item = x
 end
